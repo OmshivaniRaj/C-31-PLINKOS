274467c5c0ad2bc7616fd50cbacd3c71902ad3a8
OmshivaniRaj <omshivaniraj@gmail.com>
2021-10-05 20:08:29 +0530

    Particle.js

--- sketch.js
+++ sketch.js
@@ -15,12 +15,12 @@ function setup() {
   ground = new Ground(width/2,height,width,20);
 
   //create division objects
-  for (var k = 0; k <=80; k = k + 80) {
+  for (var k = 0; k <=800; k = k + 80) {
     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
   }
 
   //create 1st row of plinko objects
-  for (var j = 75; j <=width; j=j+50) { 
+  for (var j = 75; j <=width-05; j=j+50) { 
     plinkos.push(new Plinko(j,75));
   }
 
@@ -31,13 +31,15 @@ function setup() {
   }
 
   //create 3rd row of plinko objects
-
+    for (var j = 40; j <= width-30; j=j+50){
+    plinkos.push(new Plinko(j,275));
+  }
   
   //create 4th row of plinko objects
+   for (var j = 40; j<= width-60; j=j+50){
+     plinkos.push(new Plinko(j,375));
+   }
 
-
-  //create particle objects
-  
     
 }
  
@@ -60,6 +62,13 @@ function draw() {
     divisions[k].display();
   }
 
-  //display the paricles 
+  //create the particles using frameCount
+if (frameCount%60===0){
+ particles.push(new Particle(random(0,800),0))
+}
+  //display the particles 
+  for (var k=0; k<particles.length; k++){
+    particles[k].display();
+  }
 
 }
